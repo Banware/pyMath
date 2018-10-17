565a716fc90db7800fe495ff1c72d120430356bd
camled <38167460+Banware@users.noreply.github.com>
2018-10-17 18:47:26 -0500

    Create traArea.py

---
+++ area/trapezoid/traArea.py
@@ -0,0 +1,14 @@
+# a = (b1 - b2) * h / 2
+
+b1 = int(input("Enter b1 value: "))
+b2 = int(input("Enter b2 value: "))
+h = int(input("Enter h value: "))
+x = b1 - b2
+y = x * h
+z = y / 2
+# end variables
+
+print("1. Subract: ", x)
+print("2. Multiply: ", y)
+print("3. Divide: a = ", z)
+# end solving
