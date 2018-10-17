a4d1896c3c41d52461f7a3bf2e0c29a84737c48e
camled <38167460+Banware@users.noreply.github.com>
2018-10-17 11:38:57 -0500

    Create slope-mEquals.py

---
+++ slope/slope-mEquals.py
@@ -0,0 +1,18 @@
+#     y2 - y1
+# m = --------
+#     x2 - x1
+
+y2 = int(input("y2 value: "))
+y1 = int(input("y1 value: "))
+x2 = int(input("x2 value: "))
+x1 = int(input("x1 value: "))
+v = x2 - x1
+z = y2 - y1
+w = v / z
+# end variable input
+# the code will print out the process of solving
+
+print("1. Subtract y/x values: numerator = ", y2 - y1, "denominator = ", x2 - x1)
+print("2. Divide outcomes: ", v / z)
+print("m = ", w)
+# end solving
