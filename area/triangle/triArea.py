ad75f728ddb05b5244c628fde7730ccc52a7471c
camled <38167460+Banware@users.noreply.github.com>
2018-10-17 11:59:29 -0500

    Update triArea.py

--- area/triangle/triArea.py
+++ area/triangle/triArea.py
@@ -1,1 +1,16 @@
-# CBL
+#     h(sqrt2) * b
+# a = -------------
+#          2
+import math
+
+h = int(input("Enter h value: "))
+b = int(input("Enter b value: "))
+x = math.sqrt(h)
+y = x * b
+z = y / 2
+# end variables
+
+print("1. Find the Square root of h: ", x)
+print("2. Multiply by b: ", y)
+print("3. Divide by 2: a = ", z)
+# end solve
