0b63b27e781eb24e1a05b89c0f31eef09a83ac65
camled <38167460+Banware@users.noreply.github.com>
2018-10-17 11:45:37 -0500

    Create cirArea.py

---
+++ area/circle/cirArea.py
@@ -0,0 +1,10 @@
+# a=πr²
+
+pi = 3.14159
+r = int(input("Enter Radius: "))
+r * r = x
+# end variables
+
+print("1. Square the Radius: a = ", x)
+print("2. Multiply pi with outcome: a = ", x * x)
+# end solve
